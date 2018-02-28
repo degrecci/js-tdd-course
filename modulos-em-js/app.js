f05ea31755b462fbd51de8f281ca1da0470788ec
degrecci <hdegrecci@gmail.com>
2018-02-28 20:41:15 -0300

    Variaveis de ambiente no webpack

--- modulos-em-js/app.js
+++ modulos-em-js/app.js
@@ -2,6 +2,9 @@ import { union, uniq } from 'ramda';
 
 import sum, { sub, PI } from './utils';
 
+import react from 'react';
+import reactDOM from 'react-dom';
+
 const arr1 = [1, 1, 1, 2, 2, 3, 4, 5, 6, 6];
 const arr2 = [5, 6, 6, 6, 7, 7, 8, 9, 10, 1];
 
